0d2b1ffcdb1fa53302179eadd09f10eff13535f9
TurkeyMcMac <jwmhjwmh@gmail.com>
2018-01-14 11:12:18 -0500

    Make better use of error macros in canvas_unprint

--- src/canvas.c
+++ src/canvas.c
@@ -74,9 +74,5 @@ int canvas_print(const struct canvas *self, FILE *f)
 
 int canvas_unprint(const struct canvas *self, FILE *f)
 {
-	int total_write;
-	if CATCH_TO (total_write, fprintf,(f, "\x1B[%luA", (long unsigned)self->height))
-		return FAILURE;
-	else
-		return total_write;
+	FORWARD(fprintf,(f, "\x1B[%luA", (long unsigned)self->height));
 }
