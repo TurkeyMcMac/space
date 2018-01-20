5688abc210fdf4168b66bbd7fa67f9b359cb1e99
TurkeyMcMac <jwmhjwmh@gmail.com>
2018-01-20 08:43:14 -0500

    Remove control of seconds per frame

--- src/ticker.c
+++ src/ticker.c
@@ -5,10 +5,9 @@
 #include <errno.h>
 #include <time.h>
 
-int ticker_init(struct ticker *t, clockid_t clock_id, int sec, long nsec)
+int ticker_init(struct ticker *t, clockid_t clock_id, long interval)
 {
-	t->interval.tv_sec = sec;
-	t->interval.tv_nsec = nsec;
+	t->interval = interval;
 	t->clock_id = clock_id;
 
 	if ((errno = clock_gettime(clock_id, &t->next)) != 0) {
@@ -20,9 +19,8 @@ int ticker_init(struct ticker *t, clockid_t clock_id, int sec, long nsec)
 
 int tick(struct ticker *t)
 {
-	t->next.tv_sec += t->interval.tv_sec;
-	t->next.tv_nsec += t->interval.tv_nsec;
-	if (t->next.tv_nsec >= 1000000000) {
+	t->next.tv_nsec += t->interval;
+	while (t->next.tv_nsec >= 1000000000) {
 		t->next.tv_nsec -= 1000000000;
 		++t->next.tv_sec;
 	}
